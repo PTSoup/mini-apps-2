62ca35b921603eff6ed2f2c8a8cae6df19b0ad5b
PTSoup <phaimani.souphaphone@gmail.com>
2019-05-26 15:53:26 -0500

    [wip] Add pagination nav

--- challenge_1/client/index.jsx
+++ challenge_1/client/index.jsx
@@ -2,48 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import HistoryList from './historylist.jsx';
+import ReactPaginate from 'react-paginate';
 import './styles/styles.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [  
-                {
-                "date": "",
-                "description": "",
-                "lang": "",
-                "category1": "",
-                "category2": "",
-                "granularity": ""
-                }
-            ]
+          pageCount: 1,
+          data: [  
+              {
+              "date": "",
+              "description": "",
+              "lang": "",
+              "category1": "",
+              "category2": "",
+              "granularity": ""
+              }
+          ]
         }
-        // this.dataLoader = this.dataLoader.bind(this);
+        this.dataLoader = this.dataLoader.bind(this);
     }
 
-    // dataLoader () {
-    //     // axios.get('http://localhost:3000/events?_page=1&_limit=10&_start=1&_end=5')
-    //     //     .then((response) =>{
-    //     //         this.setState({
-    //     //             data: response
-    //     //         });
-    //     //     })
-    //     //     .catch((error) => {
-    //     //         console.log(error);
-    //     //     });
-    // }
+    dataLoader () {
+      axios.get(`http://localhost:3000/events?_page=${this.state.page}&_limit=10`)
+      .then((response) =>{
+          this.setState({
+              data: response.data
+          });
+      })
+      .catch((error) => {
+          console.log(`Error getting data`, error);
+      });
+    }
+
+    handlePageClick (event) {
+      let page = event.target;
+    }
 
     componentDidMount () {
-        axios.get('http://localhost:3000/events?_page=1&limit=2')
-        .then((response) =>{
-            this.setState({
-                data: response.data
-            });
-        })
-        .catch((error) => {
-            console.log(`Error getting data`, error);
-        });
+      this.dataLoader();
     }
 
     render () {
@@ -52,6 +50,18 @@ class App extends React.Component {
                 <h1>Behold Events in History!</h1>
                 <div id="EventList">
                     <HistoryList historyData={this.state.data}/>
+                    <ReactPaginate 
+                        previousLabel={'previous'}
+                        nextLabel={'next'}
+                        breakLabel={'...'}
+                        breakClassName={'break-me'}
+                        pageCount={3786}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        onPageChange={this.handlePageClick}
+                        containerClassName={'pagination'}
+                        subContainerClassName={'pages pagination'}
+                        activeClassName={'active'} />
                 </div>
             </div>
         )
