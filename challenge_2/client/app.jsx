3bc655e40828dded4bca4074b1005065bde59b4a
PTSoup <phaimani.souphaphone@gmail.com>
2019-05-27 09:04:41 -0500

    [C2][Setup] Complete initial setup for Challenge 2
    Completed initial setup for Challenge 2 on master branch.

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
 
+  render () {
+    return (
+      <div>Hello this is working yes?</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const port = 3000;
 
+app.use(bodyparser.urlencoded({ extended: false }));
 
+app.use(bodyparser.json());
+
+app.get('/', (req, res) => {
+    res.send(`Hello World!`)
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
