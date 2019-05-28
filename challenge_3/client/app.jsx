f444f4425d700572fe0ed5fb7d4a66bda2553c96
PTSoup <phaimani.souphaphone@gmail.com>
2019-05-27 20:31:43 -0500

    [C3][Setup] Complete initial setup for Challenge 2

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
 
+    render() {
+        return (
+            <div>
+                Hello Worldly People!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const port = 3000;
 
+app.use(bodyparser.urlencoded({ extended: false }));
 
+app.use(bodyparser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
