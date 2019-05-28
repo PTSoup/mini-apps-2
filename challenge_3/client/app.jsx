366683363714613ac7f004dc9ac2ff81fde7b0e3
PTSoup <phaimani.souphaphone@gmail.com>
2019-05-27 23:43:58 -0500

    [Release] Add UI to app
    Completed adding all UI components to the app.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -18,13 +18,18 @@ class App extends React.Component {
                 <div className="header nav">
                     <h1>The Bowling Scorer</h1>
                 </div>
+                <h4>Click on the number of pins that have been knocked down below</h4>
                 <span>
                     <PinButtons />
                 </span>
                 <div>
+                    <h2>Scoreboard</h2>
                     <Scoreboard />
                 </div>
-                
+                <div className="information">
+                    <p><strong>Current Frame:</strong> 2</p>
+                    <p><strong>Current roll:</strong> First</p>
+                </div>
             </div>
         )
     }
--- challenge_3/client/components/scoreboard.jsx
+++ challenge_3/client/components/scoreboard.jsx
@@ -4,7 +4,7 @@ class Scoreboard extends React.Component {
     render() {
         return (
             <div>
-                <div id="frame">
+                <div id="frame1" className="frame">
                     <div className="frameName">1</div>
                     <div className="scoring">
                         <div className="roll1">1</div>
@@ -12,21 +12,78 @@ class Scoreboard extends React.Component {
                     </div>
                     <div className="frameScore">3</div>
                 </div>
-                <div id="frame">
-                    <div className="frameName">1</div>
+                <div id="frame2" className="frame">
+                    <div className="frameName">2</div>
                     <div className="scoring">
-                        <div className="roll1">1</div>
-                        <div className="roll2">2</div>
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
                     </div>
-                    <div className="frameScore">3</div>
+                    <div className="frameScore"></div>
                 </div>
-                <div id="frame">
-                    <div className="frameName">1</div>
+                <div id="frame3" className="frame">
+                    <div className="frameName">3</div>
                     <div className="scoring">
-                        <div className="roll1">1</div>
-                        <div className="roll2">2</div>
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
                     </div>
-                    <div className="frameScore">3</div>
+                    <div className="frameScore"></div>
+                </div>
+                <div id="frame4" className="frame">
+                    <div className="frameName">4</div>
+                    <div className="scoring">
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
+                    </div>
+                    <div className="frameScore"></div>
+                </div>
+                <div id="frame5" className="frame">
+                    <div className="frameName">5</div>
+                    <div className="scoring">
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
+                    </div>
+                    <div className="frameScore"></div>
+                </div>
+                <div id="frame6" className="frame">
+                    <div className="frameName">6</div>
+                    <div className="scoring">
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
+                    </div>
+                    <div className="frameScore"></div>
+                </div>
+                <div id="frame7" className="frame">
+                    <div className="frameName">7</div>
+                    <div className="scoring">
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
+                    </div>
+                    <div className="frameScore"></div>
+                </div>
+                <div id="frame8" className="frame">
+                    <div className="frameName">8</div>
+                    <div className="scoring">
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
+                    </div>
+                    <div className="frameScore"></div>
+                </div>
+                <div id="frame9" className="frame">
+                    <div className="frameName">9</div>
+                    <div className="scoring">
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
+                    </div>
+                    <div className="frameScore"></div>
+                </div>
+                <div id="frame9" className="lastframe">
+                    <div className="frameName">10</div>
+                    <div className="scoring">
+                        <div className="roll1"></div>
+                        <div className="roll2"></div>
+                        <div className="roll2"></div>
+                    </div>
+                    <div className="frameScore last"></div>
                 </div>
 
             </div>
